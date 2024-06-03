8ddb17d7dd9a7531559864c5b3bde98acdf9dcfb
fernandacordeiro2006 <134058930+fernandacordeiro2006@users.noreply.github.com>
2024-06-03 14:46:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -38,16 +38,15 @@ const perguntas = [
 let atual = 0;
 let perguntaAtual;
 
-function mostraPergunta() {
+function mostraPergunta(){
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
     mostraAlternativas();
 }
-
 function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas){
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativa.textContent = alternativa;
+        botaoAlternativas.textContent = alternativa;
         caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
