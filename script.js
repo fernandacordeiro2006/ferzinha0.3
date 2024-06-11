08d98f5be570805ed4bd3eb2b12cd8e535638433
fernandacordeiro2006 <134058930+fernandacordeiro2006@users.noreply.github.com>
2024-06-11 11:29:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -17,40 +17,51 @@ const perguntas = [
                 afirmacao: "parque barigui"
             }
         ]
-},
 
     },
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia"
+        enunciado: "Depois de decidirem o lugar onde vão, chegou sabado e é hora de se arrumar, está um dia lindo de sol. Qual roupa voce escolhe usar ?",
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: "afirmacao"
+                texto: "um vestido longo rosa com um tenis branco ",
+                afirmacao: "uma calça jeans rasgada com um cropeed preto e um tenis preto"
             },
             {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "afirmacao"
+                texto: "um vestido longo rosa com um tenis branco ",
+                afirmacao: "uma calça jeans rasgada com um cropeed preto e um tenis preto"
             }
+  
         ]
-},
-
     },
     {
         enunciado: "Depois de se vestir neste dia lindo de sol. Voce prefere qual maquiagem ?",
         alternativas: [
-            "maquiagem mais basica (só corretivo de blush)",
-            "uma maquiagem mais pesada (com base, corretivo,contorno,iluminador e blush)"
+            {
+                texto: "maquiagem mais basica (só corretivo de blush)",
+                afirmacao: "uma maquiagem mais pesada (com base, corretivo,contorno,iluminador e blush)"
+            },
+            {
+                texto: "maquiagem mais basica (só corretivo de blush)",
+                afirmacao: "uma maquiagem mais pesada (com base, corretivo,contorno,iluminador e blush)"
+            }
+          
         ]
     },
     {
         enunciado: "Depois de pronta voce e sua amiga saem de casa e se divertem muito mas, derepente sentem uma fome. Voces escolhem comer no lugar onde estão ou ir para outro lugar ?",
         alternativas: [
-            "comer no lugar onde estão ",
-            "ir para outro lugar"
-        ]
-    }
-]
-
+            {
+                texto: "comer no lugar onde estão ",
+                afirmacao: "ir para outro lugar "
+            },
+            {
+                texto: "comer no lugar onde estão ",
+                afirmacao: "ir para outro lugar "
+            }
+          
+        ]  
+      }
+        
 let atual = 0;
 let perguntaAtual;
 
@@ -59,13 +70,17 @@ function mostraPergunta(){
     caixaPerguntas.textContent = perguntaAtual.enunciado;
     mostraAlternativas();
 }
+        
 function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas){
-        const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa;
-        caixaAlternativas.appendChild(botaoAlternativas);
+            const botaoAlternativas = document.createElement("button");
+            botaoAlternativas.textContent = alternativa.texto;
+            botaoAlternativas.addEventListener("click", function(){
+                atual++;
+                mostraPergunta();
+            })
+            caixaAlternativas.appendChild(botaoAlternativas);
     }
-}
 
-mostraPergunta();
-
+  
+  
